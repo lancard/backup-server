2fca9cda9c1117807df8af7730ef1dd03a7563e8
Sung-ho Kim <42758339+lancard@users.noreply.github.com>
2025-05-30 21:14:49 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,16 @@
 
 mkdir /backup
 
+ssh-keyscan -p 2222 -H mud.mywire.org > ~/.ssh/known_hosts
+ssh-keyscan -p 2222 -H fish.mywire.org >> ~/.ssh/known_hosts
+ssh-keyscan -H vatkor.net >> ~/.ssh/known_hosts
+
 while true; do
     # 복사
-    scp -r root@mud.mywire.org:/root/love.tar.gz /backup 2>&1
+    scp -P 2222 -r root@mud.mywire.org:/root/love.tar.gz /backup 2>&1
+    scp -P 2222 -r root@fish.mywire.org:/sepi/backup/backup.lib /backup 2>&1
+    scp -r root@vatkor.net:/var/www/backup/db_backup.sql.gz /backup 2>&1
 
     # 24시간 대기 (초 단위: 86400초)
-    sleep 10
+    sleep 86400
 done
